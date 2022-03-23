28bcbe386f6370a6e5bc99617a52ef74bb02d06f
karthyezhil <karthyezhi96@gmail.com>
2022-03-23 11:06:39 +0530

    karthy done his work

--- src/org/git/Sample.java
+++ src/org/git/Sample.java
@@ -1,6 +1,11 @@
 package org.git;
 
 public class Sample {
+	
+	private void m1() {
+		System.out.println("karthy code added.... ");
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Main method....  S");
 	}
